840daccb11ad04677f32d833e0f713f62ed2792a
Mek Suchi <mek@kon.in.th>
2025-07-06 14:45:43 +0700

    refactor: update certificate list and sort by importance

--- src/components/Certificate/Certificates.jsx
+++ src/components/Certificate/Certificates.jsx
@@ -4,15 +4,18 @@ import Tilt from "react-parallax-tilt";
 import { SwitchTransition, CSSTransition } from "react-transition-group";
 import styles from "./Certificate.module.css";
 
+// Portfolio ของ Suchichart Chuttale
+// Full‑Stack Junior Web Developer
+
 const certificateData = [
+  // 1. ทักษะ Web Development (Relevant to your Full‑Stack profile)
   {
     src: "https://i.imgur.com/BNX0S7z.gif",
     title: "เริ่มต้นสร้างและแสดงเนื้อหาของเว็บไซต์ด้วย HTML",
     issuedBy: "FutureSkill",
     instructor: "ธรรมฤทธิ์ ลิขิตธีรเมธ",
     completedDate: "24/11/2024",
-    certificateLink:
-      "https://app.futureskill.co/api/certificate?courseId=872&userId=209620",
+    certificateLink: "https://app.futureskill.co/api/certificate?courseId=872&userId=209620",
   },
   {
     src: "https://i.imgur.com/2F73M7J.gif",
@@ -20,173 +23,143 @@ const certificateData = [
     issuedBy: "FutureSkill",
     instructor: "ธรรมฤทธิ์ ลิขิตธีรเมธ",
     completedDate: "26/03/2025",
-    certificateLink:
-      "https://app.futureskill.co/api/certificate?courseId=873&userId=209620",
+    certificateLink: "https://app.futureskill.co/api/certificate?courseId=873&userId=209620",
   },
   {
     src: "https://i.imgur.com/UGjtqE7.gif",
-    title: "Basic​ Java​script​ for​ Frontend​ Developer​",
+    title: "Basic JavaScript for Frontend Developer",
     issuedBy: "FutureSkill",
     instructor: "ณัฏฐชัย กุลธรรมนิตย์",
     completedDate: "26/03/2025",
-    certificateLink:
-      "https://app.futureskill.co/api/certificate?courseId=516&userId=209620",
-  },
-  {
-    src: "https://i.imgur.com/XJ3YnRn.gif",
-    title: "เชื่อมต่อเว็บไซต์กับข้อมูลจากแหล่งอื่นๆ โดยใช้ API​",
-    issuedBy: "FutureSkill",
-    instructor: "ธรรมฤทธิ์ ลิขิตธีรเมธ",
-    completedDate: "26/03/2025",
-    certificateLink:
-      "https://app.futureskill.co/api/certificate?courseId=875&userId=209620",
+    certificateLink: "https://app.futureskill.co/api/certificate?courseId=516&userId=209620",
   },
   {
     src: "https://i.imgur.com/JjLXDRq.gif",
-    title: "Basic React 1 : ความรู้พื้นฐานสำ หรับ Components และ JSX​",
+    title: "Basic React 1: ความรู้พื้นฐานสำหรับ Components และ JSX",
     issuedBy: "FutureSkill",
     instructor: "ณัฏฐชัย กุลธรรมนิตย์",
     completedDate: "22/01/2025",
-    certificateLink:
-      "https://app.futureskill.co/api/certificate?courseId=466&userId=209620",
+    certificateLink: "https://app.futureskill.co/api/certificate?courseId=466&userId=209620",
   },
   {
     src: "https://i.imgur.com/wZhjJOx.gif",
-    title: "Basic React 2 : การจัดการ State และ Events​",
+    title: "Basic React 2: การจัดการ State และ Events",
     issuedBy: "FutureSkill",
     instructor: "ณัฏฐชัย กุลธรรมนิตย์",
     completedDate: "07/04/2025",
-    certificateLink:
-      "https://app.futureskill.co/api/certificate?courseId=467&userId=209620",
+    certificateLink: "https://app.futureskill.co/api/certificate?courseId=467&userId=209620",
+  },
+  {
+    src: "https://i.imgur.com/XJ3YnRn.gif",
+    title: "เชื่อมต่อเว็บไซต์กับข้อมูลจากแหล่งอื่นๆ โดยใช้ API",
+    issuedBy: "FutureSkill",
+    instructor: "ธรรมฤทธิ์ ลิขิตธีรเมธ",
+    completedDate: "26/03/2025",
+    certificateLink: "https://app.futureskill.co/api/certificate?courseId=875&userId=209620",
   },
   {
     src: "https://i.imgur.com/tm1wOzH.gif",
-    title:
-      "เริมต้นการใช้ Web Framework ด้วย Express และ รู้จักขันตอนการทํา REST API​",
+    title: "เริ่มต้นการใช้ Web Framework ด้วย Express และรู้จักขั้นตอนการทำ REST API",
     issuedBy: "FutureSkill",
     instructor: "จารุพงศ์ ปะจักโก",
     completedDate: "04/04/2025",
-    certificateLink:
-      "https://app.futureskill.co/api/certificate?courseId=380&userId=209620",
+    certificateLink: "https://app.futureskill.co/api/certificate?courseId=380&userId=209620",
   },
-  {
-    src: "https://i.imgur.com/8NbcJ0B.gif",
-    title: "Getting Started as C# Developer​",
-    issuedBy: "FutureSkill",
-    instructor: "ผู้ช่วยศาสตราจารย์ บัญญพนต์ พูลสวัสดิ์",
-    completedDate: "17/04/2025",
-    certificateLink:
-      "https://app.futureskill.co/api/certificate?courseId=308&userId=209620",
-  },
-  {
-    src: "https://i.imgur.com/IWAZ60Y.gif",
-    title: "C#: การเขียนโปรแกรมเชิงวัตถุ (Object Oriented Programming-OOP)​",
-    issuedBy: "FutureSkill",
-    instructor: "ผู้ช่วยศาสตราจารย์ บัญญพนต์ พูลสวัสดิ์",
-    completedDate: "17/04/2025",
-    certificateLink:
-      "https://app.futureskill.co/api/certificate?courseId=309&userId=209620",
-  },
-  {
-    src: "https://i.imgur.com/DeKAEyu.gif",
-    title: "Advanced For C# Programming​",
-    issuedBy: "FutureSkill",
-    instructor: "ผู้ช่วยศาสตราจารย์ บัญญพนต์ พูลสวัสดิ์",
-    completedDate: "23/04/2025",
-    certificateLink:
-      "https://app.futureskill.co/api/certificate?courseId=310&userId=209620",
-  },
-  {
-    src: "https://i.imghippo.com/files/BYg8088FSY.png",
-    title: "MOS : MS Word 23013 (12 ชั่วโมงการเรียนรู้)​",
-    issuedBy: "ThaiMOOC",
-    instructor: "อาจารย์อรทัย พรหมเพศ",
-    completedDate: "23/12/2023",
-    certificateLink:
-      "https://i.imghippo.com/files/BYg8088FSY.png",
-  },
-  {
-    src: "https://i.imghippo.com/files/kHa7320HyI.png",
-    title: "คอมพิวเตอร์กราฟิก (4 ชั่วโมงการเรียนรู้)​",
-    issuedBy: "ThaiMOOC",
-    instructor: "อาจารย์โกวิทย์ ชนะเคน",
-    completedDate: "23/12/2023",
-    certificateLink:
-      "https://i.imghippo.com/files/kHa7320HyI.png",
-  },
-  {
-    src: "https://i.imghippo.com/files/vg7871ga.png",
-    title: "พื้นฐาน Internet of Things (IoTs) (8 ชั่วโมงการเรียนรู้)​",
-    issuedBy: "ThaiMOOC",
-    instructor: "อาจารย์จักรกฤษณ์ หมั่นวิชา",
-    completedDate: "24/12/2023",
-    certificateLink:
-      "https://i.imghippo.com/files/vg7871ga.png",
-  },
-  {
-    src: "https://i.imghippo.com/files/Lnz3008gCc.png",
-    title: "การออกแบบ Infographic (6 ชั่วโมงการเรียนรู้)​",
-    issuedBy: "ThaiMOOC",
-    instructor: "อาจารย์นิพาดา ไตรรัตน์",
-    completedDate: "24/12/2023",
-    certificateLink:
-      "https://i.imghippo.com/files/Lnz3008gCc.png",
-  },
-  {
-    src: "https://i.imghippo.com/files/NXA5277YqU.png",
-    title: "Cloud Techonology Tolls เพื่อการเรียนรู้ในศตวรรษที่ 21 (6 ชั่วโมงการเรียนรู้)",
-    issuedBy: "ThaiMOOC",
-    instructor: "ดร. รัฐพล ประดับเวทย์",
-    completedDate: "25/12/2023",
-    certificateLink:
-      "https://i.imghippo.com/files/NXA5277YqU.png",
-  },
+
+  // 2. ใบรับรองจากมหาวิทยาลัยชั้นนำ (Brand Prestige)
   {
     src: "https://i.imghippo.com/files/YJbQ3876JK.jpg",
     title: "Generative AI และ ChatGPT ทำงานอย่างไร?",
-    issuedBy: "CHULA MOOC",
+    issuedBy: "CHULA MOOC (จุฬาฯ)",
     instructor: "Proadpran Punyabukkana, Ph.D.",
-    completedDate: "25/12/2023",
-    certificateLink:
-      "https://i.imghippo.com/files/YJbQ3876JK.jpg",
+    completedDate: "02/07/2025",
+    certificateLink: "https://i.imghippo.com/files/YJbQ3876JK.jpg",
   },
   {
     src: "https://i.imghippo.com/files/Tj5879xk.jpg",
-    title: "Cybersecurity Awareness for Everyone​",
-    issuedBy: "CHULA MOOC",
-    instructor: "รองศาสตราจารย์ ดร.สุวิธิดา จรุงเกียรติกุล",
-    completedDate: "25/12/2023",
-    certificateLink:
-      "https://i.imghippo.com/files/Tj5879xk.jpg",
+    title: "Cybersecurity Awareness for Everyone",
+    issuedBy: "CHULA MOOC (จุฬาฯ)",
+    instructor: "รองศาสตราจารย์ ดร. สุวิธิดา จรุงเกียรติกุล",
+    completedDate: "03/07/2025",
+    certificateLink: "https://i.imghippo.com/files/Tj5879xk.jpg",
   },
   {
     src: "https://i.imghippo.com/files/fLb9853ruc.jpg",
-    title: "Mastering AI-Powered Design & Productivity​",
-    issuedBy: "CHULA MOOC",
+    title: "Mastering AI-Powered Design & Productivity",
+    issuedBy: "CHULA MOOC (จุฬาฯ)",
     instructor: "Proadpran Punyabukkana, Ph.D.",
-    completedDate: "25/12/2023",
-    certificateLink:
-      "https://i.imghippo.com/files/fLb9853ruc.jpg",
+    completedDate: "04/07/2025",
+    certificateLink: "https://i.imghippo.com/files/fLb9853ruc.jpg",
   },
   {
+    src: "https://i.imghippo.com/files/rbhg7193akQ.jpg",
+    title: "การใช้ Microsoft Excel สำหรับการทำงานสำนักงาน",
+    issuedBy: "มหาวิทยาลัยเกษตรศาสตร์",
+    instructor: "รองศาสตราจารย์ ดร. วีระเกษตร สวนผกา",
+    completedDate: "06/07/2025",
+    certificateLink: "https://i.imghippo.com/files/rbhg7193akQ.jpg",
+  },
+
+  // 3. ทักษะรองอื่นๆ ที่เสริมภาพรวม
+  {
     src: "https://i.imghippo.com/files/SRj4361zfc.jpg",
-    title: "ได้เข้าร่วมอบรมเชิงปฏิบัติการเรื่อง การเขียนทางวิชาการและการเขียนอ้างอิง",
-    issuedBy: "สำนักวิทยบริการและเทคโนโลยีสารสนเทศ มหาวิทยาลัยราชภัฏนครราชสีมา",
+    title: "อบรมเชิงปฏิบัติการ การเขียนทางวิชาการและการเขียนอ้างอิง",
+    issuedBy: "สำนักวิทยบริการฯ ม.ราชภัฏนครราชสีมา",
     instructor: "ผู้ช่วยศาสตราจารย์ ภณิดา แก้วกูร",
     completedDate: "10/01/2024",
-    certificateLink:
-      "https://i.imghippo.com/files/SRj4361zfc.jpg",
+    certificateLink: "https://i.imghippo.com/files/SRj4361zfc.jpg",
   },
   {
     src: "https://i.imghippo.com/files/OYT4029vs.png",
-    title: "ได้ผ่านการทดสอบวัดทักษะ DIGITAL LITERACY​",
-    issuedBy: "สำนักคอมพิวเตอร์ มหาวิทยาลัยราชภัฏนครราชสีมา",
+    title: "ผ่านการทดสอบวัดทักษะ DIGITAL LITERACY",
+    issuedBy: "สำนักคอมพิวเตอร์ ม.ราชภัฏนครราชสีมา",
     instructor: "ผู้ช่วยศาสตราจารย์ ดร. อดิศร เนาวนนท์",
     completedDate: "10/01/2024",
-    certificateLink:
-      "https://i.imghippo.com/files/OYT4029vs.png",
+    certificateLink: "https://i.imghippo.com/files/OYT4029vs.png",
+  },
+  {
+    src: "https://i.imghippo.com/files/BYg8088FSY.png",
+    title: "MOS : MS Word 23013 (12 ชั่วโมงการเรียนรู้)",
+    issuedBy: "ThaiMOOC",
+    instructor: "อาจารย์อรทัย พรหมเพศ",
+    completedDate: "23/12/2023",
+    certificateLink: "https://i.imghippo.com/files/BYg8088FSY.png",
+  },
+  {
+    src: "https://i.imghippo.com/files/kHa7320HyI.png",
+    title: "คอมพิวเตอร์กราฟิก (4 ชั่วโมงการเรียนรู้)",
+    issuedBy: "ThaiMOOC",
+    instructor: "อาจารย์โกวิทย์ ชนะเคน",
+    completedDate: "23/12/2023",
+    certificateLink: "https://i.imghippo.com/files/kHa7320HyI.png",
+  },
+  {
+    src: "https://i.imghippo.com/files/vg7871ga.png",
+    title: "พื้นฐาน Internet of Things (IoTs) (8 ชั่วโมงการเรียนรู้)",
+    issuedBy: "ThaiMOOC",
+    instructor: "อาจารย์จักรกฤษณ์ หมั่นวิชา",
+    completedDate: "24/12/2023",
+    certificateLink: "https://i.imghippo.com/files/vg7871ga.png",
+  },
+  {
+    src: "https://i.imghippo.com/files/Lnz3008gCc.png",
+    title: "การออกแบบ Infographic (6 ชั่วโมงการเรียนรู้)",
+    issuedBy: "ThaiMOOC",
+    instructor: "อาจารย์นิพาดา ไตรรัตน์",
+    completedDate: "24/12/2023",
+    certificateLink: "https://i.imghippo.com/files/Lnz3008gCc.png",
+  },
+  {
+    src: "https://i.imghippo.com/files/NXA5277YqU.png",
+    title: "Cloud Technology Tools เพื่อการเรียนรู้ในศตวรรษที่ 21 (6 ชั่วโมงการเรียนรู้)",
+    issuedBy: "ThaiMOOC",
+    instructor: "ดร. รัฐพล ประดับเวทย์",
+    completedDate: "25/12/2023",
+    certificateLink: "https://i.imghippo.com/files/NXA5277YqU.png",
   },
 ];
+
+
 
 function Certificate() {
   const { t } = useTranslation();
